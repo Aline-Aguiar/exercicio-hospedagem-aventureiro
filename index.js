8e56d8b7e06b3bbe68dd8f1d33eb9cb1ffc7b452
Aline-Aguiar <aline.aglilly@gmail.com>
2025-01-12 09:55:01 -0300

    alterando o nome da variável de msgInicial para vaiSeHospedar

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const msgInicial = confirm('Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?')
+const vaiSeHospedar = confirm('Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?')
 
-if(msgInicial){
+if(vaiSeHospedar){
     alert('Ótimo! Nós temos as melhores camas de toda a região!')
 }else{
     alert('Que pena! Você parecia ser uma pessoa mais legal')
